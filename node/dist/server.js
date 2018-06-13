ef9a28ace0103345d52c91cc6c8acb80fcbc6067
bucci <bucci@dhcp-206-87-134-93.ubcsecure.wireless.ubc.ca>
2018-06-13 12:31:38 -0700

    logging enabled

--- node/dist/server.js
+++ node/dist/server.js
@@ -9,6 +9,8 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+const fs = require('fs');
+
 server.listen(3000);
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -42,13 +44,6 @@ io.on('connection', (client) => {
 // Arduino code
 //
 ////////////////////////////////////////////////////////////////////////////////
-
-function message(msg) {
-	if (msg.voltage > 0) {
-		console.log("A" + msg.sensor + ": \t" + msg.voltage);
-		io.emit('sensor', msg)
-	}
-}
 var board = new five.Board({
 		repl: false,
 	});
@@ -62,21 +57,84 @@ board.on("ready", function() {
 	this.pinMode(2, five.Pin.ANALOG);
 	this.pinMode(3, five.Pin.ANALOG);
 	this.pinMode(4, five.Pin.ANALOG);
+	this.pinMode(5, five.Pin.DIGITAL);
 
 	this.analogRead(0, function(voltage) {
-		message({sensor: 0, voltage: voltage});
+		handleMessage({sensor: 0, voltage: voltage});
 	});
 	this.analogRead(1, function(voltage) {
-		message({sensor: 1, voltage: voltage});
+		handleMessage({sensor: 1, voltage: voltage});
 	});
 	this.analogRead(2, function(voltage) {
-		message({sensor: 2, voltage: voltage});
+		handleMessage({sensor: 2, voltage: voltage});
 	});
 	this.analogRead(3, function(voltage) {
-		message({sensor: 3, voltage: voltage});
+		handleMessage({sensor: 3, voltage: voltage});
 	});
 	this.analogRead(4, function(voltage) {
-		message({sensor: 4, voltage: voltage});
+		handleMessage({sensor: 4, voltage: voltage});
 	});
+	this.digitalRead(5, function(voltage) {
+		handleMessage({sensor: 5, voltage: voltage})
+	})
+
 	io.emit("ready","the board is ready");
 });
+
+////////////////////////////////////////////////////////////////////////////////
+// IO code
+//
+////////////////////////////////////////////////////////////////////////////////
+var state = {
+	A0: -1,
+	A1: -1,
+	A2: -1,
+	A3: -1,
+	A4: -1,
+	D5: -1
+}
+
+var logTimer;
+var session = Date.now();
+var filepath = './logs/log_' + session + '.txt';
+var delimiter = ",";
+
+fs.open(filepath, 'w', (err, fd) => {
+	if (err) {
+		return console.error(err);
+	}
+	fs.close(fd, (err) => {
+		if (err) throw err;
+	});
+	console.log("Opened " + filepath + " successfully!");
+	var header = ["A0", "A1", "A2", "A3", "A4", "D5", "timestamp"];
+	log(header.join(delimiter));
+	logTimer = setInterval(function(){
+		var now = Date.now();
+		var arr = [state.A0, state.A1, state.A2, state.A3, state.A4, state.D5, now];
+		var msg = arr.join(delimiter);
+		log(msg);
+	}, 1);
+});
+
+function message(msg) {
+	if (msg.voltage > 0) {
+		console.log("A" + msg.sensor + ": \t" + msg.voltage);
+		updateSensors(msg);
+		io.emit('sensor', msg)
+	}
+}
+
+// Unpack message to update the message state
+function updateSensors(msg) {
+	state[msg.sensor] = msg.voltage;
+}
+
+// MSG is a string???
+function log(msg) {
+	fs.appendFile(filepath, msg + "\n", function (err) {
+	  if (err) throw err;
+	  // console.log('Saved!');
+	});
+}
+
