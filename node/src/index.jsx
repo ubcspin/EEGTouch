0de3b7b9035fdae2cbc92da9ce611938253adfec
bucci <bucci@dhcp-206-87-134-93.ubcsecure.wireless.ubc.ca>
2018-06-13 16:58:53 -0700

    working bar graph

--- node/src/index.jsx
+++ node/src/index.jsx
@@ -1,11 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Video from "./Video.jsx"
+
+
 import { subscribeToTimer, subscribeToSensor, subscribeToJoystick, emit } from './api';
-// require `react-d3-core` for Chart component, which help us build a blank svg and chart title.
-var Chart = require('react-d3-core').Chart;
-// require `react-d3-basic` for Line chart component.
-var LineChart = require('react-d3-basic').LineChart;
+
+import { letterFrequency } from '@vx/mock-data';
+import { Group } from '@vx/group';
+import { Bar } from '@vx/shape';
+import { scaleLinear, scaleBand } from '@vx/scale';
+
+
+// We'll use some mock data from `@vx/mock-data` for this.
+const data = letterFrequency;
+
+// Define the graph dimensions and margins
+const width = 500;
+const height = 500;
+const margin = { top: 20, bottom: 20, left: 20, right: 20 };
+
+// Then we'll create some bounds
+const xMax = width - margin.left - margin.right;
+const yMax = height - margin.top - margin.bottom;
+
+// We'll make some helpers to get at the data we want
+const x = d => d.letter;
+const y = d => +d.frequency * 100;
+
+// And then scale the graph by our data
+const xScale = scaleBand({
+  rangeRound: [0, xMax],
+  domain: data.map(x),
+  padding: 0.4,
+});
+const yScale = scaleLinear({
+  rangeRound: [yMax, 0],
+  domain: [0, Math.max(...data.map(y))],
+});
+
+// Compose together the scale and accessor functions to get point functions
+const compose = (scale, accessor) => (data) => scale(accessor(data));
+const xPoint = compose(xScale, x);
+const yPoint = compose(yScale, y);
+
+
+// Finally we'll embed it all in an SVG
+function BarGraph(props) {
+  return (
+    <svg width={width} height={height}>
+      {data.map((d, i) => {
+        const barHeight = yMax - yPoint(d);
+        return (
+          <Group key={`bar-${i}`}>
+            <Bar
+              x={xPoint(d)}
+              y={yMax - barHeight}
+              height={barHeight}
+              width={xScale.bandwidth()}
+              fill='#fc2e1c'
+            />
+          </Group>
+        );
+      })}
+    </svg>
+  );
+}
+
 
 class Hello extends React.Component {
 	constructor(props) {
@@ -93,22 +153,8 @@ class Hello extends React.Component {
             This is the Sensor A{this.state.sensor} value: {this.state.voltage}
           </p>
         </div>
-        <Chart
-      title={title}
-      width={width}
-      height={height}
-      margins= {margins}
-      >
-      <LineChart
-        margins= {margins}
-        title={title}
-        data={chartData}
-        width={width}
-        height={height}
-        chartSeries={chartSeries}
-        x={x}
-      />
-    </Chart>
+        <BarGraph />
+        
       </div>
 		)
 	};
