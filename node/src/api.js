9edeb375cf1d2b1bff6ddf8e2061469f251f12f0
laurarodgers <30274095+laurarodgers@users.noreply.github.com>
2018-06-28 04:36:17 -0700

    Cleaned up api
    Removed unused or unnecessary code.
    renamed subscribe as only joystick values are being sent to client

--- node/src/api.js
+++ node/src/api.js
@@ -1,21 +1,12 @@
 import openSocket from 'socket.io-client';
 const socket = openSocket('http://localhost:3000');
 
-function subscribeToTimer(cb) {
-  socket.on('timer', timestamp => cb(null, timestamp));
-  socket.emit('subscribeToTimer', 1000);
-}
-
-function subscribeToSensor(cb)  {
-  socket.on('sensor', sensor => cb(null, sensor));
-  // socket.emit('subscribeToTimer', 1000);
+function subscribeToJoystick(cb)  {
+  socket.on('joystick', joystickReading => cb(null, joystickReading));
 }
 
 function emit(header, msg) {
 	socket.emit(header, msg);
 }
-// function subscribeToJoystick(cb) {
-// 	socket.on('joystickUpdate', joystickVal => cb(null, joystickVal));
-// }
 
-export { subscribeToTimer, subscribeToSensor, emit };
+export { subscribeToJoystick, emit };
