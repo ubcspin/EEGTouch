92b86996cf40440e7708b3c8650ac79a921e22bd
laurarodgers <30274095+laurarodgers@users.noreply.github.com>
2018-07-11 18:23:48 -0700

    better smoothing
    Added better algorithm to smooth jitter. It's big and unnecessarily stateful and ugly but it works!

--- node/src/index.jsx
+++ node/src/index.jsx
@@ -9,25 +9,40 @@ class ReplayPage extends React.Component {
 
       subscribeToJoystick((err, joystickReading) => {
         // sanity check omits values outside of reasonable hardware range
-        // shoudl be recalibrated on each hardware change
+        // should be recalibrated on each hardware change
         if (true) { //(joystickReading > 100 && joystickReading < 500) {
+          //this.setState({joystickStabReading: Math.round(joystickReading/2 + this.state.joystickStabReading/2)});
           this.setState({joystickPosition: joystickReading});
           var arr = this.state.joystickVals;
           arr.push(joystickReading);
           this.setState({joystickVals: arr});
-          if (this.state.joystickVals.length >= 10) {
-            var total = 0;
-            for (var i = 1; i <= 5; i++) {
-              total += this.state.joystickVals[this.state.joystickVals.length - i];
-            }
-            this.setState({joystickStabReading:  Math.trunc(total/5)});
-          }
-          else {
-            this.setState({joystickStabReading: this.state.joystickPosition});
-          }
+          var stab = joystickReading;
+          this.responsiveAnalogRead(joystickReading);
+          console.log('stab: ' + this.state.joystickStabReading);
+          // if (this.state.joystickVals.length >= 10) {
+          //   var sample = this.state.joystickVals.slice(this.state.joystickVals.length-5, this.state.joystickVals.length);
+          //   sample.sort();
+          //   stab = sample[2];
+          // }
+          // stab = Math.round(stab/2 + this.state.joystickStabReading/2)
+          // this.setState({joystickStabReading: stab})
+
+          //stab = 2*Math.round(stab/2);
+          //this.setState({joystickStabReading: stab});
+
+     //       var total = 0;
+     //       for (var i = 1; i <= 7; i++) {
+     //         total += this.state.joystickVals[this.state.joystickVals.length - i];
+     //       }
+     //       this.setState({joystickStabReading:  2*Math.round(Math.round(total/7)/2)});
+     //       console.log('stab: ' + this.state.joystickStabReading);
+     //}
+     //     else {
+     //      this.setState({joystickStabReading: this.state.joystickPosition});
+     //     }
           // console.log(this.state.joystickVals);
 
-          // joyRelHeight: relative position of joystick from ~(1 to 100)
+          // joyRelPosition: relative position of joystick from ~(1 to 100)
           // used to calculate height, color of visualization
           // calculated based on min/max positions for analog input
           // should be recalibrated on each hardware change
@@ -73,9 +88,15 @@ class ReplayPage extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
 
     this.state = {
+
+      //layout states
       revealVideo: false,
       doneJump: false,
       playback: false,
+      whichButton: false,
+      vidJumpMin: 0,
+      vidJumpSec: 0,
+      debug: true,
 
       // raw joystick and video data kept for debugging
       joystickPosition: -1,
@@ -87,9 +108,18 @@ class ReplayPage extends React.Component {
       videoWidth: 1096,
       videoHeight: 616,
 
-      whichButton: false,
-      vidJumpMin: 0,
-      vidJumpSec: 0,
+      //smoothing constants
+      SNAP_MULTIPLIER: 0.01,
+      SLEEP_ENABLE: true,
+      ACTIVITY_THRESHOLD: 2,
+      EDGE_SNAP_ENABLE: true,
+      ANALOG_RESOLUTION: 1024,
+
+      //smoothing variables
+      smoothValue: 0,
+      errorEMA: 0,
+      //lastActivityMS: 0,
+      sleeping: false,
 
       // initialize default style for joystick bar
       joyBar: {
@@ -231,7 +261,7 @@ class ReplayPage extends React.Component {
     this.setState({doneJump: true});
     event.preventDefault();
     var timeSet = parseInt(this.state.vidJumpMin)*60 + parseInt(this.state.vidJumpSec);
-    this.refs.myVideo.currentTime = timeSet; 
+    this.refs.myVideo.currentTime = timeSet;
     this.setState({progressBar : {
            width: this.state.videoWidth * timeSet
             / this.refs.myVideo.duration,
@@ -267,6 +297,98 @@ class ReplayPage extends React.Component {
   else {return (<div />);}
   }
 
+  debugInfo() {
+  if (this.state.debug == true) {
+    return(
+      <div className='CentreBottomPosition'>
+        <p>Joystick: {this.state.joystickPosition} </p>
+      </div>)
+  }
+  else {
+  return <div />
+  }
+  }
+
+  // // //
+  //Responsive analog read algorithm from http://damienclarke.me/code/posts/writing-a-better-noise-reducing-analogread
+  //TLDR: fixing jitter while maintaining responsive visual is not a trivial problem.
+  //Straight median, mean and exponential filters all introduce too much lag whenever they're strong
+  //enough to actually kill stationary jitter.
+  //This more complicated solution does work though.
+  responsiveAnalogRead(newVal) {
+  // get current milliseconds
+  // does this get used anywhere?
+  //var ms = millis();
+  var newValue = newVal;
+
+  // if sleep and edge snap are enabled and the new value is very close to an edge, drag it a little closer to the edges. This'll make it easier to pull the output values right to the extremes without sleeping, and it'll make movements right near the edge appear larger, making it easier to wake up.
+  if(this.state.SLEEP_ENABLE && this.state.EDGE_SNAP_ENABLE) {
+    if(newValue < this.state.ACTIVITY_THRESHOLD) {
+      newValue = newValue*2 - this.state.ACTIVITY_THRESHOLD;
+    } else if(newValue > this.state.ANALOG_RESOLUTION - this.state.ACTIVITY_THRESHOLD) {
+      newValue = newValue*2 - this.state.ANALOG_RESOLUTION + this.state.ACTIVITY_THRESHOLD;
+    }
+  }
+
+  // get difference between new input value and current smooth value
+  var diff = Math.abs(newValue - this.state.smoothValue);
+
+  // measure the difference between the new value and current value over time
+  // to get a more reasonable indication of how far off the current smooth value is
+  // compared to the actual measurements
+  this.setState({errorEMA: this.state.errorEMA + ((newValue - this.state.smoothValue) - this.state.errorEMA) * 0.4});
+
+  // if sleep has been enabled, keep track of when we're asleep or not by marking the time of last activity and testing to see how much time has passed since then
+  if(this.state.SLEEP_ENABLE) {
+    // recalculate sleeping status
+    // (asleep if last activity was over SLEEP_DELAY_MS ago)
+    this.setState({sleeping: Math.abs(this.state.errorEMA) < this.state.ACTIVITY_THRESHOLD});
+  }
+
+  // if we're allowed to sleep, and we're sleeping
+  // then don't update responsiveValue this loop
+  // just output the existing responsiveValue
+  if(this.state.SLEEP_ENABLE && this.state.sleeping) {
+    return Math.floor(this.state.smoothValue);
+  }
+
+  // now calculate a 'snap curve' function, where we pass in the diff (x) and get back a number from 0-1. We want small values of x to result in an output close to zero, so when the smooth value is close to the input value it'll smooth out noise aggressively by responding slowly to sudden changes. We want a small increase in x to result in a much higher output value, so medium and large movements are snappy and responsive, and aren't made sluggish by unnecessarily filtering out noise. A hyperbola (f(x) = 1/x) curve is used. First x has an offset of 1 applied, so x = 0 now results in a value of 1 from the hyperbola function. High values of x tend toward 0, but we want an output that begins at 0 and tends toward 1, so 1-y flips this up the right way. Finally the result is multiplied by 2 and capped at a maximum of one, which means that at a certain point all larger movements are maximally snappy
+
+  var snapCurve = function(x) {
+    var y = 1 / (x + 1);
+    y = (1 - y)*2;
+    if(y > 1) {
+      return 1;
+    }
+    return y;
+  };
+
+  // multiply the input by SNAP_MULTIPLER so input values fit the snap curve better.
+  var snap = snapCurve(diff * this.state.SNAP_MULTIPLIER);
+
+  // when sleep is enabled, the emphasis is stopping on a responsiveValue quickly, and it's less about easing into position. If sleep is enabled, add a small amount to snap so it'll tend to snap into a more accurate position before sleeping starts.
+  if(this.state.SLEEP_ENABLE) {
+    snap = snap*0.5 + 0.5;
+  }
+
+  // (update globalSnap so we can show snap in the output window)
+  //globalSnap = snap;
+
+  // calculate the exponential moving average based on the snap
+  this.setState({smoothValue: this.state.smoothValue + (newValue - this.state.smoothValue) * snap});
+
+  // ensure output is in bounds
+  if(this.state.smoothValue < 0) {
+    this.setState({smoothValue : 0});
+  } else if(this.state.smoothValue > this.state.ANALOG_RESOLUTION - 1) {
+    this.setState({smoothValue: this.state.ANALOG_RESOLUTION - 1});
+  }
+
+  // expected output is an integer
+  //return Math.floor(smoothValue);
+  this.setState({joystickStabReading: this.state.smoothValue});
+}
+  // // //
 
 
   render() {
@@ -305,14 +427,11 @@ class ReplayPage extends React.Component {
         <div className='JoystickBg' />
         <div className='JoyBar' style={this.state.joyBar} />
 
+        {this.debugInfo()}
+
       </div>
     )
   };
 }
 
 ReactDOM.render(<ReplayPage/>, document.getElementById('replayPage'));
-
-
-// code to explicitly render joystick, video time values for debugging:
-//        <div className='CentreBottomPosition'>
-//        <p> Joystick: {this.state.joystickPosition}</p></div>
