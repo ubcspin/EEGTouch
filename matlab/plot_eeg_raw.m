b81f0ed8fbe27bf023ec568f3b60252980ec81d9
Laura Rodgers <laurarodgers@gmail.com>
2019-02-06 13:09:27 -0800

    fixed plot eeg raw

--- matlab/plot_eeg_raw.m
+++ matlab/plot_eeg_raw.m
@@ -1,30 +1,25 @@
-%plots 64 channels of eeg data with random colors, for some reason
-f = waitbar(0.3,'Plotting EEG data','Name','Data Processing');
+% plots 64 channels of eeg data with standard graph colors
 
-if (exist('eeg_data_synced') ~= 0)
-    eeg_to_plot = eeg_data_synced(2:end,:);
+load_globals;
 
-    en = length(eeg_to_plot(1,:))-100000;
-    ax = ones(1,length(eeg_to_plot(1,:)))/60000;
-    ax = cumsum(ax);
-    colors = rand(64,3);
-    hold on;
-    k = 1;
-    while k < 20
-        plot(ax(1:en),eeg_to_plot(k,1:en),'Color',colors(k,:));
-        k = k+1;
-    end
-    hold off;
-
-    xlabel('Time (min)');
-    ylabel('microvolts');
-    %title('64-channel EEG potentials during gameplay');
-    hold off;
-    saveas(gcf,fullfile(processed_directory,'eeg.png'));
-else
-    close(f);
-    waitfor(warndlg('No EEG series found. Aborting.'));
-    clearvars f;
+if exist('processed_data','var') && ~any(ismember(fields(processed_data),{'eeg'}))
+   eeg_align; 
 end
-    close(f);
-    clearvars f en ax colors k eeg_to_plot;
+
+eeg = processed_data.eeg.eeg;
+ms = processed_data.eeg.timestamp_ms;
+min = ms/60000;
+hold on;
+k = 1;
+NUM_CHANNELS = 64;
+while k < NUM_CHANNELS
+    plot(min, eeg(:,k));
+    k = k+1;
+end
+hold off;
+
+xlabel('Time (min)');
+title(['64-channel EEG potentials during gameplay for trial ' trial_number]);
+saveas(gcf,fullfile(processed_directory,'eeg.png'));
+
+clearvars eeg k min ms NUM_CHANNELS;
